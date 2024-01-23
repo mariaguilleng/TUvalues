4b2c244629a226b486241a1c7e463309ad04fcff
itsmeryguillen <meryguillen10@gmail.com>
2024-01-23 12:46:12 +0100

    ESD value

--- R/equal_surplus_division.R
+++ R/equal_surplus_division.R
@@ -14,6 +14,7 @@
 
 equal_surplus_division <- function(value_func, n_players = 0) {
 
+  browser()
   esd_value <- rep(0, n_players)
   if (class(value_func) == "numeric") {
 
@@ -23,10 +24,10 @@ equal_surplus_division <- function(value_func, n_players = 0) {
       value_func <- c(0, value_func)
       n_players<-log(length(value_func+1),2)
     }
-
+    value_func <- value_func[-1]
     individual_sum <- sum(value_func[1:n_players])
     for(i in 1:n_players) {
-      esd_value[i] <- value_func[i] + (value_func[length(value_func)] + individual_sum)/n_players
+      esd_value[i] <- value_func[i] + (value_func[length(value_func)] - individual_sum)/n_players
     }
 
   } else if (class(value_func) == "function") {
@@ -36,7 +37,7 @@ equal_surplus_division <- function(value_func, n_players = 0) {
     }
     individual_sum <- sum(sapply(1:n_players, value_func))
     for(i in 1:n_players) {
-      esd_value[i] <- value_func(i) + (value_func(1:n_players) + individual_sum)/n_players
+      esd_value[i] <- value_func(i) + (value_func(1:n_players) - individual_sum)/n_players
     }
   }
   names(esd_value) <- 1:n_players
